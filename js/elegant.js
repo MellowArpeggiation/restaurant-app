369914dc7ad54337f4672be288a45a8a45daff28
George Paton <georgewpaton@me.com>
2015-08-10 13:02:32 +1000

    eleganter
    Too many changes for a good commit message

---
+++ js/elegant.js
@@ -0,0 +1,35 @@
+/*jslint browser: true, devel: true*/
+
+/*global $*/
+
+/*global swapInit, swapElegant*/
+
+
+function swapInit() {
+	'use strict';
+	
+	$(".ele-swap").each(function () {
+		setInterval(swapElegant, 50, this);
+	});
+}
+
+function swapElegant(element) {
+	'use strict';
+	var createdString = "";
+	
+	if (Math.random() > 0.08) {
+		return;
+	}
+	
+	createdString += (Math.random() > 0.5 ? "E" : "3");
+	createdString += (Math.random() > 0.5 ? "l" : "1");
+	createdString += (Math.random() > 0.5 ? "e" : "3");
+	createdString += "gant";
+	
+	$(element).html(createdString);
+}
+
+$(document).ready(function () {
+	'use strict';
+	swapInit();
+});
